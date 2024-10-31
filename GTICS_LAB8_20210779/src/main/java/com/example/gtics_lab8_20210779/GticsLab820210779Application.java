cdbdcb3e192f354c7c30a6c5797826f494e0e3aa
Lia Mariel Arancibia Castillo <a20210779@pucp.edu.pe>
2024-10-30 19:36:43 -0500

    pom

---
+++ GTICS_LAB8_20210779/src/main/java/com/example/gtics_lab8_20210779/GticsLab820210779Application.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab8_20210779;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GticsLab820210779Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GticsLab820210779Application.class, args);
+	}
+
+}
---
+++ GTICS_LAB8_20210779/src/test/java/com/example/gtics_lab8_20210779/GticsLab820210779ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics_lab8_20210779;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GticsLab820210779ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
